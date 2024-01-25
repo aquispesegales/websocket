9db47cbe5659ab1df5c3c2c6cd8820631d6f743b
aquispesegales <alvaroquispesegales@gmail.com>
2024-01-24 23:18:47 -0400

    quitando ssl

--- src/PagoQrController.ts
+++ src/PagoQrController.ts
@@ -7,7 +7,7 @@ export class PagoQrController {
     @Get('/notificar/:alias')
     NotificaPagoQr(@Param('alias') alias: string) {
 
-        const socket = io("wss://localhost:9082");
+        const socket = io("ws://localhost:9082");
         socket.on('connect',()=>{
             socket.emit("confirmaPagoQr", alias);
         })
