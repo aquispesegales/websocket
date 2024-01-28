8e78a8100e2ed371c4abccea8dca5dd12f2c1336
aquispesegales <alvaroquispesegales@gmail.com>
2024-01-28 08:03:33 -0400

    ss

--- src/websockets/websoclet.gateway.ts
+++ src/websockets/websoclet.gateway.ts
@@ -11,7 +11,7 @@ import {
 import { Server,Socket } from "socket.io";
 
 
-@WebSocketGateway(9082,{ cors: true })
+@WebSocketGateway(9083,{ cors: true })
 export class WebsocketGateway implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit {
     afterInit(client: Socket) {
         console.log("Socket iniciado");
